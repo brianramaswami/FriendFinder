c02dff36e56122b05feb44350d84d42f109ebdd5
brianramaswami <brianramaswami@yahoo.com>
2017-06-19 19:57:56 -0700

    pretty much done

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,7 +3,7 @@ module.exports = (app,path)=>{
         res.sendFile(path.join(__dirname,'../public/home.html'));
     });
 
-    app.get('/friends',(req,res)=>{
+    app.get('/survey',(req,res)=>{
         res.sendFile(path.join(__dirname,'../public/survey.html'));
     });
 };
